c63c1f4cb623db0b2b7dbb31e12bdac474058c6b
Mikami <kaz7sense@hotmail.com>
2023-02-27 23:41:33 -0400

    My custom hooks

---
+++ useTodo/useTodo.js
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from 'react';
+// Reducers
+import todoReducer from './todoReducer';
+
+const init = () => JSON.parse(localStorage.getItem('state')) || [];
+
+const useTodo = (initialState = []) => {
+  const [state, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('state', JSON.stringify(state));
+  }, [state]);
+
+  const onAddTodo = (todo) => {
+    const action = {
+      type: 'ADD',
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const onDeleteTodo = (id) => {
+    const action = {
+      type: 'REMOVE',
+      payload: id,
+    };
+
+    dispatch(action);
+  };
+
+  const onToggleTodo = (id) => {
+    const action = {
+      type: 'TOGGLE',
+      payload: id,
+    };
+
+    dispatch(action);
+  };
+
+  return {
+    state,
+    ...state,
+    onAddTodo,
+    onToggleTodo,
+    onDeleteTodo,
+  };
+};
+
+export default useTodo;
